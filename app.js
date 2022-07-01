0df70243d34db9573d9823cb860171636d6378d2
Taylor Yamashita <taylory@princeton.edu>
2022-07-01 13:10:39 -0700

    Initialize board upon load, set up basic board coordinates

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 function initializeBoard() {
     for (let i = 0; i < 6; i++) {
-        document.getElementById("game-board").innerHTML += `<div id="row-${i}" style="display:inline-block">${i}</div>`;
+        document.getElementById("game-board").innerHTML += `<div class="row" id="row-${i}"></div>`;
     }
     for (let i = 0; i < 6; i++) {
         for (let j = 0; j < 7; j++) {
-            // print(i + ", " + j);
-            document.getElementById(`row-${i}`).innerHTML += `<div>${j}</div>`;
+            document.getElementById(`row-${i}`).innerHTML += `<div class="col">${i}, ${j}</div>`;
         }
     }
 }
