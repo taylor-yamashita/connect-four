83b411635c36a5be762a9e69d725431a4f18b78a
Taylor Yamashita <taylory@princeton.edu>
2022-07-06 10:36:43 -0700

    add global to track if currently playing, add listeners when initializing board to detect when colmns are clicked

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+// global variables
+let inGame = false;
+
+// gameplay functions
 function initializeBoard() {
-    for (let j = 0; j < 7; j++) {
-        document.getElementById("game-board").innerHTML += `<div class="col" id="col-${j}"></div>`;
+    for (let i = 0; i < 7; i++) {
+        document.getElementById("game-board").innerHTML += `<div class="col" id="col-${i}" onclick="turn(${i})"></div>`;
     }
-    for (let j = 0; j < 7; j++) {
-        for (let i = 0; i < 6; i++) {
-            document.getElementById(`col-${j}`).innerHTML += `<div class="cell">${j}, ${i}</div>`;
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 6; j++) {
+            document.getElementById(`col-${i}`).innerHTML += `<div class="cell">${i}, ${j}</div>`;
         }
     }
 }
 
 function startGame() {
-    console.log("inside start game function")
+    inGame = true;
 }
 
+const turn = (col) => {
+    if (inGame) {
+        console.log("col " + col + " clicked");
+    }
+}
