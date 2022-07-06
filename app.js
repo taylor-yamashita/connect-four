d4d6c6487e0dd7da9faaf7b7ca8c7d7fa41f4730
Taylor Yamashita <taylory@princeton.edu>
2022-07-06 10:43:15 -0700

    add toggle between start and quit game buttons

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let inGame = false;
 // gameplay functions
 function initializeBoard() {
     for (let i = 0; i < 7; i++) {
-        document.getElementById("game-board").innerHTML += `<div class="col" id="col-${i}" onclick="turn(${i})"></div>`;
+        document.getElementById("game-board").innerHTML += `<div class="col" id="col-${i}" onclick="colClicked(${i})"></div>`;
     }
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < 6; j++) {
@@ -14,10 +14,24 @@ function initializeBoard() {
 }
 
 function startGame() {
+    // set global variables
     inGame = true;
+
+    // swap start and quit game buttons
+    document.getElementById("startBtn").classList.add("hidden");
+    document.getElementById("quitBtn").classList.remove("hidden");
 }
 
-const turn = (col) => {
+function quitGame() {
+    // set global variables
+    inGame = false;
+
+    // swap start and quit game buttons
+    document.getElementById("quitBtn").classList.add("hidden");
+    document.getElementById("startBtn").classList.remove("hidden");
+}
+
+const colClicked = (col) => {
     if (inGame) {
         console.log("col " + col + " clicked");
     }
