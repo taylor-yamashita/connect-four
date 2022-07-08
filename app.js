bc3bacea3b815ed0279fe258d9de03ccc1861ccf
Taylor Yamashita <taylory@princeton.edu>
2022-07-08 09:43:42 -0700

    cleaning + reformatting code

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ let inGame = false;
 let playerWon = false;
 let currentPlayer = 1;
 
-// gameplay functions
 function initializeBoard() {
+    // create columns
     for (let i = 0; i < 7; i++) {
-        document.getElementById("game-board").innerHTML += `<div class="col" id="col-${i}" onclick="colClicked(${i})"></div>`;
+        document.getElementById("game-board").innerHTML += `<div class="col" id="col-${i}" onclick="playerMove(${i})"></div>`;
     }
+    // create board cells within columns
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < 6; j++) {
             document.getElementById(`col-${i}`).innerHTML += `<div class="cell" id="cell-${i}-${j}"></div>`;
@@ -19,21 +20,29 @@ function startGame() {
     // set global variables for new game
     inGame = true;
     currentPlayer = 1;
-    playerClicked = false;
 
     // swap start and quit game buttons
     document.getElementById("start-btn").classList.add("hidden");
     document.getElementById("quit-btn").classList.remove("hidden");
 
-    // show current player
-    document.getElementById("current-player-display").classList.remove("hidden");
+    // show current player display
+    document.getElementById("player-display").classList.remove("hidden");
 }
 
-const colClicked = (col) => {
+function quitGame() {
+    // set global variables
+    inGame = false;
+
+    // swap start and quit game buttons
+    document.getElementById("quit-btn").classList.add("hidden");
+    document.getElementById("start-btn").classList.remove("hidden");
+
+    // hide current player display
+    document.getElementById("player-display").classList.add("hidden");
+}
+
+const playerMove = (col) => {
     if (inGame) {
-
-        playerClicked = true;
-
         // piece drop animation
         const loop = async () => {
             let localCurrentPlayer = currentPlayer;
@@ -59,7 +68,7 @@ const colClicked = (col) => {
             currentPlayer = 1;
         }
 
-        // update current player
+        // update current player display
         if (currentPlayer === 1) {
             document.getElementById("p1-turn-dot").style.backgroundColor = "palevioletred";
             document.getElementById("p2-turn-dot").style.backgroundColor = "white";
@@ -67,19 +76,7 @@ const colClicked = (col) => {
             document.getElementById("p1-turn-dot").style.backgroundColor = "white";
             document.getElementById("p2-turn-dot").style.backgroundColor = "yellow";
         }
+
+        // check for player win; if win, handle end game
     }
 }
-
-function quitGame() {
-    // set global variables
-    inGame = false;
-
-    // swap start and quit game buttons
-    document.getElementById("quit-btn").classList.add("hidden");
-    document.getElementById("start-btn").classList.remove("hidden");
-
-    // hide current player
-    document.getElementById("current-player-display").classList.add("hidden");
-}
-
-
